66044db032595b03ec7e9e4e3f3b19b265b8e496
MiHarsh <harshm17172612@gmail.com>
2022-05-08 09:41:19 +0530

    drowsiness and alignment Apis are live

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+# from requests import request
 from imutils import paths  # imutils includes opencv functions
 import face_recognition
 import pickle
@@ -39,9 +40,7 @@ todo_ref = db.collection('todos')
 EYE_AR_THRESH = 0.30
 EYE_AR_CONSEC_FRAMES = 45
 
-# initialize the frame counters and the total number of blinks
-COUNTER = 0
-ALARM_ON = False
+
 
 # grab the indexes of the facial landmarks for the left and
 # right eye, respectively
@@ -193,6 +192,8 @@ def get_mapped_ss(frame, face_locations, face_ids_present):
 
 detector, predictor = get_attention_models()
 mtx = get_calibrateCameraMatrix()
+
+
 @app.route('/add', methods=['POST'])
 def create():
     """
@@ -206,6 +207,8 @@ def create():
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
+
+
 @app.route('/list', methods=['GET'])
 def read():
     """
@@ -215,7 +218,7 @@ def read():
     """
     try:
         # Check if ID was passed to URL query
-        todo_id = request.args.get('id')    
+        todo_id = request.args.get('id')
         if todo_id:
             todo = todo_ref.document(todo_id).get()
             return jsonify(todo.to_dict()), 200
@@ -224,6 +227,8 @@ def read():
             return jsonify(all_todos), 200
     except Exception as e:
         return f"An Error Occured: {e}"
+
+
 @app.route('/update', methods=['POST', 'PUT'])
 def update():
     """
@@ -237,6 +242,8 @@ def update():
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
+
+
 @app.route('/delete', methods=['GET', 'DELETE'])
 def delete():
     """
@@ -298,145 +305,162 @@ def mark_attendance():
     return face_ids_present
 
 
-@app.route("/facealignment")
+@app.route("/facealignment", methods=["POST"])
 def face_alignment():
     """
     Input:
         Link - class recording
     """
-    cap = cv2.VideoCapture('video.webm')
 
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    duration = frame_count/fps
+    try:
+        print("url is", request.json['url'])
+        video_url = request.json['url']
+        cap = cv2.VideoCapture(video_url)
 
-    print(f"Duration of the recording- {duration} sec")
-    print(f"Total Frames count: {frame_count}")
-    print(f"Frames per sec: {fps}")
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        duration = frame_count/fps
 
-    frame_cnt = 0
-    store = []
-    start_duration = 0
+        print(f"Duration of the recording- {duration} sec")
+        print(f"Total Frames count: {frame_count}")
+        print(f"Frames per sec: {fps}")
 
-    LST_GAZE = "Face Not Found"
-    while True:
-        GAZE = "Face Not Found"
+        frame_cnt = 0
+        store = []
+        start_duration = 0
 
-        ret, img = cap.read()
+        LST_GAZE = "Face Not Found"
+        while True:
+            GAZE = "Face Not Found"
 
-        # if no frame found
-        if not ret:
-            break
+            ret, img = cap.read()
 
-        faces = detector(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), 0)
-        face3Dmodel = ref3DModel()
+            # if no frame found
+            if not ret:
+                break
 
-        for face in faces:
-            shape = predictor(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), face)
-            refImgPts = ref2DImagePoints(shape)
-            mdists = np.zeros((4, 1), dtype=np.float64)
+            faces = detector(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), 0)
+            face3Dmodel = ref3DModel()
 
-            success, rotationVector, translationVector = cv2.solvePnP(
-                face3Dmodel, refImgPts, mtx, mdists)
+            for face in faces:
+                shape = predictor(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), face)
+                refImgPts = ref2DImagePoints(shape)
+                mdists = np.zeros((4, 1), dtype=np.float64)
 
-            noseEndPoints3D = np.array([[0, 0, 1000.0]], dtype=np.float64)
-            noseEndPoint2D, jacobian = cv2.projectPoints(
-                noseEndPoints3D, rotationVector, translationVector, mtx, mdists)
+                success, rotationVector, translationVector = cv2.solvePnP(
+                    face3Dmodel, refImgPts, mtx, mdists)
 
-            rmat, jac = cv2.Rodrigues(rotationVector)
-            angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+                noseEndPoints3D = np.array([[0, 0, 1000.0]], dtype=np.float64)
+                noseEndPoint2D, jacobian = cv2.projectPoints(
+                    noseEndPoints3D, rotationVector, translationVector, mtx, mdists)
 
-            if angles[1] < -15:
-                GAZE = "Looking: Left"
-            elif angles[1] > 15:
-                GAZE = "Looking: Right"
-            else:
-                GAZE = "Forward"
+                rmat, jac = cv2.Rodrigues(rotationVector)
+                angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
 
-            if GAZE != LST_GAZE:
+                if angles[1] < -15:
+                    GAZE = "Looking: Left"
+                elif angles[1] > 15:
+                    GAZE = "Looking: Right"
+                else:
+                    GAZE = "Forward"
 
-                if LST_GAZE != "Forward":
-                    store.append([LST_GAZE, frame_cnt/fps - start_duration])
+                if GAZE != LST_GAZE:
 
-                start_duration = frame_cnt/fps
-                # print(GAZE, "starting time(sec):", start_duration ,  "sec")
-            LST_GAZE = GAZE
+                    if LST_GAZE != "Forward":
+                        store.append(
+                            [LST_GAZE, frame_cnt/fps - start_duration])
 
-        frame_cnt += 1
-    return store
+                    start_duration = frame_cnt/fps
+                    # print(GAZE, "starting time(sec):", start_duration ,  "sec")
+                LST_GAZE = GAZE
+
+            frame_cnt += 1
+        return jsonify({"drowsiness_alerts": store})
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 
-@app.route("/drowsiness")
+@app.route("/drowsiness", methods=["POST"])
 def get_drowsiness():
-    cap = cv2.VideoCapture('video.webm')
 
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    duration = frame_count/fps
+    try:
+        print("url is", request.json['url'])
+        video_url = request.json['url']
+        cap = cv2.VideoCapture(video_url)
 
-    print(f"Duration of the recording- {duration} sec")
-    print(f"Total Frames count: {frame_count}")
-    print(f"Frames per sec: {fps}")
+        # initialize the frame counters and the total number of blinks
+        COUNTER = 0
+        ALARM_ON = False
 
-    store = []
-    frame_cnt = 0
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        duration = frame_count/fps
 
-        frame = imutils.resize(frame, width=500)
+        print(f"Duration of the recording- {duration} sec")
+        print(f"Total Frames count: {frame_count}")
+        print(f"Frames per sec: {fps}")
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        store = []
+        frame_cnt = 0
+        while True:
+            success, frame = cap.read()
+            if not success:
+                break
 
-        # detect faces in the grayscale image
-        rects = detector(gray, 0)
+            frame = imutils.resize(frame, width=500)
 
-        # loop over the face detections
-        for rect in rects:
-            # determine the facial landmarks for the face region, then
-            # convert the facial landmark (x, y)-coordinates to a NumPy
-            # array
-            shape = predictor(gray, rect)
-            shape = face_utils.shape_to_np(shape)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            # extract the left and right eye coordinates, then use the
-            # coordinates to compute the eye aspect ratio for both eyes
-            leftEye = shape[lStart:lEnd]
-            rightEye = shape[rStart:rEnd]
-            leftEAR = eye_aspect_ratio(leftEye)
-            rightEAR = eye_aspect_ratio(rightEye)
+            # detect faces in the grayscale image
+            rects = detector(gray, 0)
 
-            # average the eye aspect ratio together for both eyes
-            ear = (leftEAR + rightEAR) / 2.0
+            # loop over the face detections
+            for rect in rects:
+                # determine the facial landmarks for the face region, then
+                # convert the facial landmark (x, y)-coordinates to a NumPy
+                # array
+                shape = predictor(gray, rect)
+                shape = face_utils.shape_to_np(shape)
 
-            # check to see if the eye aspect ratio is below the blink
-            # threshold, and if so, increment the blink frame counter
-            if ear < EYE_AR_THRESH:
-                COUNTER += 1
-                if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                    # if the alarm is not on, turn it on
-                    if not ALARM_ON:
-                        ALARM_ON = True
+                # extract the left and right eye coordinates, then use the
+                # coordinates to compute the eye aspect ratio for both eyes
+                leftEye = shape[lStart:lEnd]
+                rightEye = shape[rStart:rEnd]
+                leftEAR = eye_aspect_ratio(leftEye)
+                rightEAR = eye_aspect_ratio(rightEye)
 
-                        # check to see if an alarm file was supplied,
-                        # and if so, start a thread to have the alarm
-                        # sound played in the background
+                # average the eye aspect ratio together for both eyes
+                ear = (leftEAR + rightEAR) / 2.0
 
-                        store.append(["Drowsiness", frame_cnt/fps])
+                # check to see if the eye aspect ratio is below the blink
+                # threshold, and if so, increment the blink frame counter
+                if ear < EYE_AR_THRESH:
+                    COUNTER += 1
+                    if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                        # if the alarm is not on, turn it on
+                        if not ALARM_ON:
+                            ALARM_ON = True
 
-                        # print("Drowsiness", "time(sec):", frame_cnt/fps,  "sec")
+                            # check to see if an alarm file was supplied,
+                            # and if so, start a thread to have the alarm
+                            # sound played in the background
 
-                # otherwise, the eye aspect ratio is not below the blink
-                # threshold
-            else:
+                            store.append(["Drowsiness", frame_cnt/fps])
 
-                # reset the eye frame counter
-                COUNTER = 0
-                ALARM_ON = False
-        frame_cnt += 1
+                            # print("Drowsiness", "time(sec):", frame_cnt/fps,  "sec")
 
-    return store
+                    # otherwise, the eye aspect ratio is not below the blink
+                    # threshold
+                else:
+
+                    # reset the eye frame counter
+                    COUNTER = 0
+                    ALARM_ON = False
+            frame_cnt += 1
+
+        return jsonify({"drowsiness_alerts": store})
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 
 # Create arrays of known face encodings and their names
